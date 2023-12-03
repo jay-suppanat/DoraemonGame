973936a4d6322f26b2d000f6104f8e6fa35b5110
jay-suppanat <s.chinthumrucks@gmail.com>
2023-12-03 15:35:38 +0700

    Refactor code

--- DoraemonGame/DoraemonGame/Controller/CommandController/CommandViewController.swift
+++ DoraemonGame/DoraemonGame/Controller/CommandController/CommandViewController.swift
@@ -97,66 +97,66 @@ extension CommandViewController: UITableViewDelegate {
     }
 
     let okAction = UIAlertAction(title: "OK", style: .default) { [weak self] action in
-               if let textField = alert.textFields?.first,
-                  let newText = textField.text {
-                 switch indexPath.row {
-                 case 0:
-                   UserDefault.shared.setCommand2(command2: newText)
-                   self?.commandArray[indexPath.row] = newText
-                   self?.tableView.reloadRows(at: [indexPath], with: .automatic)
-                 case 1:
-                   UserDefault.shared.setCommand3(command3: newText)
-                   self?.commandArray[indexPath.row] = newText
-                   self?.tableView.reloadRows(at: [indexPath], with: .automatic)
-                 case 2:
-                   UserDefault.shared.setCommand4(command4: newText)
-                   self?.commandArray[indexPath.row] = newText
-                   self?.tableView.reloadRows(at: [indexPath], with: .automatic)
-                 case 3:
-                   UserDefault.shared.setCommand5(command5: newText)
-                   self?.commandArray[indexPath.row] = newText
-                   self?.tableView.reloadRows(at: [indexPath], with: .automatic)
-                 case 4:
-                   UserDefault.shared.setCommand6(command6: newText)
-                   self?.commandArray[indexPath.row] = newText
-                   self?.tableView.reloadRows(at: [indexPath], with: .automatic)
-                 case 5:
-                   UserDefault.shared.setCommand7(command7: newText)
-                   self?.commandArray[indexPath.row] = newText
-                   self?.tableView.reloadRows(at: [indexPath], with: .automatic)
-                 case 6:
-                   UserDefault.shared.setCommand8(command8: newText)
-                   self?.commandArray[indexPath.row] = newText
-                   self?.tableView.reloadRows(at: [indexPath], with: .automatic)
-                 case 7:
-                   UserDefault.shared.setCommand9(command9: newText)
-                   self?.commandArray[indexPath.row] = newText
-                   self?.tableView.reloadRows(at: [indexPath], with: .automatic)
-                 case 8:
-                   UserDefault.shared.setCommand10(command10: newText)
-                   self?.commandArray[indexPath.row] = newText
-                   self?.tableView.reloadRows(at: [indexPath], with: .automatic)
-                 case 9:
-                   UserDefault.shared.setCommandJ(commandJ: newText)
-                   self?.commandArray[indexPath.row] = newText
-                   self?.tableView.reloadRows(at: [indexPath], with: .automatic)
-                 case 10:
-                   UserDefault.shared.setCommandQ(commandQ: newText)
-                   self?.commandArray[indexPath.row] = newText
-                   self?.tableView.reloadRows(at: [indexPath], with: .automatic)
-                 case 11:
-                   UserDefault.shared.setCommandK(commandK: newText)
-                   self?.commandArray[indexPath.row] = newText
-                   self?.tableView.reloadRows(at: [indexPath], with: .automatic)
-                 case 12:
-                   UserDefault.shared.setCommandA(commandA: newText)
-                   self?.commandArray[indexPath.row] = newText
-                   self?.tableView.reloadRows(at: [indexPath], with: .automatic)
-                 default:
-                   print("🚨 Change Command Error")
-                 }
-               }
-           }
+      if let textField = alert.textFields?.first,
+         let newText = textField.text {
+        switch indexPath.row {
+        case 0:
+          UserDefault.shared.setCommand2(command2: newText)
+          self?.commandArray[indexPath.row] = newText
+          self?.tableView.reloadRows(at: [indexPath], with: .automatic)
+        case 1:
+          UserDefault.shared.setCommand3(command3: newText)
+          self?.commandArray[indexPath.row] = newText
+          self?.tableView.reloadRows(at: [indexPath], with: .automatic)
+        case 2:
+          UserDefault.shared.setCommand4(command4: newText)
+          self?.commandArray[indexPath.row] = newText
+          self?.tableView.reloadRows(at: [indexPath], with: .automatic)
+        case 3:
+          UserDefault.shared.setCommand5(command5: newText)
+          self?.commandArray[indexPath.row] = newText
+          self?.tableView.reloadRows(at: [indexPath], with: .automatic)
+        case 4:
+          UserDefault.shared.setCommand6(command6: newText)
+          self?.commandArray[indexPath.row] = newText
+          self?.tableView.reloadRows(at: [indexPath], with: .automatic)
+        case 5:
+          UserDefault.shared.setCommand7(command7: newText)
+          self?.commandArray[indexPath.row] = newText
+          self?.tableView.reloadRows(at: [indexPath], with: .automatic)
+        case 6:
+          UserDefault.shared.setCommand8(command8: newText)
+          self?.commandArray[indexPath.row] = newText
+          self?.tableView.reloadRows(at: [indexPath], with: .automatic)
+        case 7:
+          UserDefault.shared.setCommand9(command9: newText)
+          self?.commandArray[indexPath.row] = newText
+          self?.tableView.reloadRows(at: [indexPath], with: .automatic)
+        case 8:
+          UserDefault.shared.setCommand10(command10: newText)
+          self?.commandArray[indexPath.row] = newText
+          self?.tableView.reloadRows(at: [indexPath], with: .automatic)
+        case 9:
+          UserDefault.shared.setCommandJ(commandJ: newText)
+          self?.commandArray[indexPath.row] = newText
+          self?.tableView.reloadRows(at: [indexPath], with: .automatic)
+        case 10:
+          UserDefault.shared.setCommandQ(commandQ: newText)
+          self?.commandArray[indexPath.row] = newText
+          self?.tableView.reloadRows(at: [indexPath], with: .automatic)
+        case 11:
+          UserDefault.shared.setCommandK(commandK: newText)
+          self?.commandArray[indexPath.row] = newText
+          self?.tableView.reloadRows(at: [indexPath], with: .automatic)
+        case 12:
+          UserDefault.shared.setCommandA(commandA: newText)
+          self?.commandArray[indexPath.row] = newText
+          self?.tableView.reloadRows(at: [indexPath], with: .automatic)
+        default:
+          print("🚨 Change Command Error")
+        }
+      }
+    }
 
     alert.addAction(cancelAction)
     alert.addAction(okAction)
